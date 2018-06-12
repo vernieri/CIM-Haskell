ac891167779fd3a96695a08670da4b365fdf9932
Vernieri <31966345+vernieri@users.noreply.github.com>
2018-06-11 21:53:04 -0300

    Update Loja.hs

--- src/Handler/Loja.hs
+++ src/Handler/Loja.hs
@@ -23,3 +23,62 @@ getLojaR tid = do
                 Valor: #{instrumentoValor instrumento}
               
         |]
+
+postLojaR :: InstrumentoId -> Handler Html
+postLojaR tid = do
+    runDB $ delete tid
+    redirect ListaLojaR
+
+getListaLojaR :: Handler Html
+getListaLojaR = do
+    instrumento <- runDB $ selectList [] [Asc InstrumentoNome]
+    defaultLayout $ do
+        addStylesheetRemote "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"
+        addStylesheetRemote "https://fonts.googleapis.com/icon?family=Material+Icons"
+        addScriptRemote "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js"
+        addScriptRemote "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.css"
+        setTitle "CIM"        
+        toWidget $(luciusFile "templates/rapidao.lucius")        
+
+        [whamlet|
+
+            <div class="container">
+                <div class="enter">
+                        <div class="row">
+                            <div class="col s12 m8 offset-m2 offset-l1 l10 z-depth-1 white opacity-95 border-circle">
+                                <div class="section">          
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th>
+                                                    <h2>Nome
+
+                                                <th>
+                                                    <h2>Estado
+
+                                                <th>
+                                                    <h2>Valor
+
+                                                <th>
+                                                    <form action=@{InstrumentoR} method="get">
+                                                        <input type="submit" value="Adicionar!">
+
+
+                                        <tbody>
+                                            $forall (Entity tid instrumento) <- instrumento
+                                                <tr>
+                                                    <td>
+                                                        <a href=@{LojaR tid}>
+                                                            #{instrumentoNome instrumento}
+
+                                                    <td>
+                                                        #{show $ instrumentoTipo instrumento}
+
+                                                    <td>
+                                                        #{instrumentoValor instrumento}
+                                                    
+                                                    <td>
+                                                        <form action=@{LojaR tid} method=post>
+                                                            <input type="submit" value="Comprar!">
+        |]
+
